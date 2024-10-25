550f16cba17cf50654627cbeab8c378f61f76c95
gulu864 <suva.kanta4u@gmail.com>
2024-10-25 19:03:21 +0530

    Add files via upload

---
+++ Nested_loops.py
@@ -0,0 +1,5 @@
+for i in range(1,11):
+
+    for x in range(1,21):
+        print(x , end=' ')
+    print()
---
+++ Nested_loops_even.py
@@ -0,0 +1,6 @@
+for i in range(1,11):
+
+    for a in range(0,21,2):
+
+        print(a, end="  ")
+    print()
